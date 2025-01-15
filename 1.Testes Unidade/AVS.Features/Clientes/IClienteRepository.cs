e1b8ab35c74eee0956f6188f3e2c151e831384ab
Alexander Vieira <alexander.silva.developer@gmail.com>
2025-01-15 17:58:31 -0300

    chore[testes-software]: Testes de unidade - Implementação testes de unidade avançado.

---
+++ 1.Testes Unidade/AVS.Features/Clientes/IClienteRepository.cs
@@ -0,0 +1,9 @@
+﻿using AVS.Features.Core;
+
+namespace AVS.Features.Clientes
+{
+    public interface IClienteRepository : IRepository<Cliente>
+    {
+        Cliente ObterPorEmail(string email);
+    }
+}
